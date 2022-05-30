c277598ae4f4847be6e60a34d00c9285326f8ac5
Fogel Egor <fogel_92@bk.ru>
2022-05-31 01:59:04 +0700

    Remote repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock name="Мексиканская" price={500} />
+            <PizzaBlock name="Пепперони" price={695} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>
+          Все
+        </li>
+        <li onClick={() => onClickCategory(1)} className={activeIndex === 1 ? 'active' : ''}>
+          Мясные
+        </li>
+        <li onClick={() => onClickCategory(2)} className={activeIndex === 2 ? 'active' : ''}>
+          Вегетарианская
+        </li>
+        <li onClick={() => onClickCategory(3)} className={activeIndex === 3 ? 'active' : ''}>
+          Гриль
+        </li>
+        <li onClick={() => onClickCategory(4)} className={activeIndex === 4 ? 'active' : ''}>
+          Острые
+        </li>
+        <li onClick={() => onClickCategory(5)} className={activeIndex === 5 ? 'active' : ''}>
+          Закрытые
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
