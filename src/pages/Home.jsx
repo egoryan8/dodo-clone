7f429ffb812695e24d42d0002855871c475a91f2
Fogel Egor <fogel_92@bk.ru>
2022-06-01 02:49:09 +0700

    feat: add media requests and carts page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,11 @@ const Home = () => {
         setPizzas(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -30,7 +31,7 @@ const Home = () => {
           ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
           : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
 
