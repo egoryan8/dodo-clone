bcae442415162aebe89bca53aa37e1158ddd5e7e
Fogel Egor <fogel_92@bk.ru>
2022-05-31 16:29:56 +0700

    feat: add fetch request to mocapi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React from 'react';
+
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 import './scss/app.scss';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://6295d76075c34f1f3b2280f4.mockapi.io/pizzas')
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setPizzas(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
